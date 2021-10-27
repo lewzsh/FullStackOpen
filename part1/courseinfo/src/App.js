359ce04a1771ddc0ab460b26dd858ac72fac5050
lewis <casibila@gmail.com>
2021-10-27 16:13:38 -0700

    init commit

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.number}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+
+      <Content />
+      
+      <Total number={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
